d585d36448576b7ed44eebe5a0692497546e0543
Greg <34096711+gregoryreyes@users.noreply.github.com>
2024-02-24 20:54:50 -0500

    feat: adding bottom section to footer, adding styles for tex transformation on divs

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,18 +2,25 @@ import { Link } from 'react-router-dom';
 // import Tos from '../components/Tos';
 
 export default function Footer() {
+
+  const year = new Date().getFullYear();
+  console.log( 'year --> ',year );
   return(
     <div>
       
-      <div className="hwuFooterContent">
+      <div className='hwuFooterContent'>
         <div>
-          <Link to="/contact">Contact Us</Link>
+          <Link to='/contact'>Contact Us</Link>
         </div>
         <div>
           <Link to='/tos'>
             Terms Of Service
           </Link>
         </div>
+      </div>
+
+      <div className='hwuFooterBottomWrap'>
+        © {year} hewitus.com
       </div>
       
     </div>
